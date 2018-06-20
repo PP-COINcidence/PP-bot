4bc76b7ab04664f594b04ffc30b0ee12d1f36ef1
PP-COINcidence <40278033+PP-COINcidence@users.noreply.github.com>
2018-06-20 18:57:26 +0200

    Update ppbot.py

--- ppbot.py
+++ ppbot.py
@@ -13,35 +13,40 @@ def on_message(message):
     if message.channel.id == '456515271964753930':
         if message.content.startswith("!dispo"):
             yield from client.delete_message(message)
-        if message.content.startswith("!dispo"):
-            message = yield from client.send_message(discord.Object(id='456515389741072386'), str(message.content)[6:])
+            message = yield from client.send_message(discord.Object(id='456515389741072386'), "**"+str(message.content)[6:]+"**")
             yield from client.add_reaction(message,'\u2705')
             yield from client.add_reaction(message,'\u274C')
         if message.content.startswith("!clean"):
+            yield from client.delete_message(message)
             yield from client.purge_from(discord.Object(id='456515389741072386'), limit=100, check=None, before=None, after=None, around=None)
+            an="**"+str(message.author)+"** a nettoyé le channel préparation-ccfn \U0001F5D1"
+            yield from client.send_message(discord.Object(id='456515271964753930'),an)
 
 @client.event
 @asyncio.coroutine
 def on_reaction_add(reaction,user):
     if reaction.message.channel.id == '456515389741072386':
-        if "CCFN" in reaction.message.content:
-            if "[P²]#4011" not in str(user):
-                if str(reaction.emoji) == "✅":
-                    an="**"+str(user)+"** est disponible pour le prochain CCFN ! :smiley:"
-                    yield from client.send_message(discord.Object(id='456515271964753930'),an)
-                    return
-        if not "CCFN" in reaction.message.content:
-            if "[P²]#4011" not in str(user):
-                if str(reaction.emoji) == "✅":
-                    an="**"+str(user)+"** est disponible pour le prochain event ! :smiley:"
-                    yield from client.send_message(discord.Object(id='456515271964753930'),an)
+        if "[P²]#4011" not in str(user):
+            if str(reaction.emoji) == "✅":
+                if str(user) not in reaction.message.content:
+                    yield from client.edit_message(reaction.message, new_content=reaction.message.content+"\n-"+str(user)+" in")
 
+@client.event
+@asyncio.coroutine
+def on_reaction_remove(reaction, user):
+    if reaction.message.channel.id == '456515389741072386':
+        if "[P²]#4011" not in str(user):
+            if str(reaction.emoji) == "✅":
+                if "\n-"+str(user)+" in" in reaction.message.content:
+                    lol = reaction.message.content
+                    an = lol.replace("\n-"+str(user)+" in", " ")
+                    yield from client.edit_message(reaction.message, new_content=an)
 @client.event
 @asyncio.coroutine
 def on_voice_state_update(before,after):
     if before.voice_channel != after.voice_channel:
         if after.voice_channel.id == '456057790579081220':
-            yield from client.send_message(discord.Object(id='456515271964753930'),"Besoin d'un @Admin")
+            yield from client.send_message(discord.Object(id='456515271964753930'),"**"+str(after)+"** a besoin d'un @Admin \U0001F198")
  
 @client.event
 @asyncio.coroutine
