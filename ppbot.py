081ddc80a62c565454cbccb94b15a23b05a742ff
PP-COINcidence <40278033+PP-COINcidence@users.noreply.github.com>
2018-10-30 22:45:22 +0100

    Update ppbot.py

--- ppbot.py
+++ ppbot.py
@@ -172,8 +172,9 @@ def on_reaction_remove(reaction, user):
 @asyncio.coroutine
 def on_voice_state_update(before,after):
     if before.voice_channel != after.voice_channel:
-        if after.voice_channel == discord.Object(id='436860873882075151'):
-            yield from client.send_message(discord.Object(id='441518062575943680'),"<@"+str(after.id)+"> a besoin d'un <@&440560633696616459> \U0001F198")
+        for channel in after.server.channels:
+            if channel.id == '436860873882075151':
+                yield from client.send_message(discord.Object(id='441518062575943680'),"<@"+str(after.id)+"> a besoin d'un <@&440560633696616459> \U0001F198")
 
 @client.event
 @asyncio.coroutine
